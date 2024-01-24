5b2ce99a2fa60cd81d06c7b8a1f6965a819c0393
mpabhinavakumar <114505064+mpabhinavakumar@users.noreply.github.com>
2024-01-24 20:41:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+img = "";
+status = "";
+objects = [];
+
+
+function setup() {
+
+
+    canvas = createCanvas(640,420);
+    canvas.center(); 
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting objects";
+}
+
+
+
+function preload(){
+img = loadImage('dog_cat.jpg');
+}
+
+function draw () {
+    image(img,0,0,640,420);
+        if (status != "")
+        {
+            for ( i = 0; i <  objects.length; i++)
+            {
+                document.getElementById("status").innerHTML = "status : objects detected";
+                
+                fill("red");
+                percent = floor(objects[i].confidence * 100);
+                text(objects[i].label + " "+ percent + "%",objects[i].x+15, objects[i].y+15);
+                noFill();
+                stroke("red");
+                rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+             }
+        }
+
+   
+}
+
+function gotResult(error,results)
+{
+    if (error) {
+
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function modelLoaded() {
+
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img,gotResult);
+}
